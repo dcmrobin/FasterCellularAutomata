72fa926ecaa827890209fb5fd19451a7b5c7813d
DavidCM <dcm.robin@gmail.com>
2024-04-24 20:11:44 +0100

    okay. the thread racing is gone.

--- Assets/Scripts/ConwayCompute.cs
+++ Assets/Scripts/ConwayCompute.cs
@@ -5,44 +5,50 @@ public class ConwayCompute : MonoBehaviour
     public ComputeShader computeShader;
     public int width = 300;
     public int height = 300;
-    public RenderTexture renderTexture;
+    public RenderTexture renderTexture1;
+    public RenderTexture renderTexture2;
+    private bool useRenderTexture1 = true;
 
     void Start()
     {
-        InitializeTexture();
+        InitializeTextures();
         RandomInitialize();
     }
 
-    void InitializeTexture()
+    void InitializeTextures()
     {
-        renderTexture = new RenderTexture(width, height, 0, RenderTextureFormat.RFloat);
-        renderTexture.enableRandomWrite = true;
-        renderTexture.Create();
+        renderTexture1 = new RenderTexture(width, height, 0, RenderTextureFormat.RFloat);
+        renderTexture1.enableRandomWrite = true;
+        renderTexture1.Create();
+
+        renderTexture2 = new RenderTexture(width, height, 0, RenderTextureFormat.RFloat);
+        renderTexture2.enableRandomWrite = true;
+        renderTexture2.Create();
     }
 
     void RandomInitialize()
     {
-        computeShader.SetTexture(1, "Result", renderTexture);
+        computeShader.SetTexture(1, "currentBuffer", useRenderTexture1 ? renderTexture1 : renderTexture2);
+        computeShader.SetTexture(1, "nextBuffer", useRenderTexture1 ? renderTexture2 : renderTexture1);
         computeShader.Dispatch(1, width / 16, height / 16, 1);
-    }
-
-    void OnDestroy()
-    {
-        renderTexture.Release();
     }
 
     private void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
-        if (renderTexture == null)
-        {
-            renderTexture = new RenderTexture(width, height, 24);
-            renderTexture.enableRandomWrite = true;
-            renderTexture.Create();
-        }
+        computeShader.SetTexture(0, "currentBuffer", useRenderTexture1 ? renderTexture1 : renderTexture2);
+        computeShader.SetTexture(0, "nextBuffer", useRenderTexture1 ? renderTexture2 : renderTexture1);
+        computeShader.Dispatch(0, width / 16, height / 16, 1);
 
-        computeShader.SetTexture(0, "Result", renderTexture);
-        computeShader.Dispatch(0, renderTexture.width / 16, renderTexture.height / 16, 1);
+        // Swap render textures for the next frame
+        useRenderTexture1 = !useRenderTexture1;
 
-        Graphics.Blit(renderTexture, dest);
+        // Display the result using Graphics.Blit
+        Graphics.Blit(useRenderTexture1 ? renderTexture1 : renderTexture2, dest);
+    }
+
+    void OnDestroy()
+    {
+        renderTexture1.Release();
+        renderTexture2.Release();
     }
 }
