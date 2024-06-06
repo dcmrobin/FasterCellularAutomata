1eb2a9e138707dfe037923f67a3f57a4b64dae07
DavidCM <dcm.robin@gmail.com>
2024-06-06 13:39:04 +0100

    implemented Frogs automaton

--- Assets/Scripts/ConwayCompute.cs
+++ Assets/Scripts/ConwayCompute.cs
@@ -348,6 +348,10 @@ public class ConwayCompute : MonoBehaviour
                 type = new float4(1, 0.2f, 0.2f, 1);
                 currentRuleText.text = "3458/37/4";
                 break;
+            case 24:
+                type = new float4(0, 0.8f, 0, 0.4f);
+                currentRuleText.text = "12/34/3";
+                break;
             default:
                 type = new float4(0, 0, 0, 1);
                 currentRuleText.text = "ERROR/ERROR";
